e76d455c318141b30c0da15340211719d0a8f7f0
Vitaliy Levashin <Vitaliy_Levashin@rcomext.com>
2016-08-17 18:16:28 +0300

    JsonTest

--- src/test/groovy/lang_features/JsonTest.groovy
+++ src/test/groovy/lang_features/JsonTest.groovy
@@ -1,6 +1,5 @@
 package lang_features
 
-import groovy.json.JsonParserType
 import groovy.json.JsonSlurper
 import org.junit.Test
 
@@ -8,11 +7,10 @@ class JsonTest {
 
     @Test
     void shouldParseJson() {
-        def jsonSlurper = new JsonSlurper(type: JsonParserType.INDEX_OVERLAY)
-        def object = jsonSlurper.parseText('{ "myList": [4, 8, 15, 16, 23, 42] }')
-
+        def jsonSlurper = new JsonSlurper()
+        def object = jsonSlurper.parseText('{ "myList": [4, 8, 15] }')
         assert object instanceof Map
         assert object.myList instanceof List
-        assert object.myList == [4, 8, 15, 16, 23, 42]
+        assert object.myList == [4, 8, 15]
     }
 }
